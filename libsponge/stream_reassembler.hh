167f64b96122603d5d48e56850039f5ff5892921
dream <2036019675@qq.com>
2024-07-27 14:46:26 +0800

    之前使用区间合并来写, 现在换了一种新方法用bitmap来标记每一个位, 并且通过来所有测试 Changes to be committed:       modified:   libsponge/stream_reassembler.cc       modified:   libsponge/stream_reassembler.hh

--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -3,8 +3,9 @@
 
 #include "byte_stream.hh"
 
+#include <cstddef>
 #include <cstdint>
-#include <map>
+#include <deque>
 #include <string>
 
 //! \brief A class that assembles a series of excerpts from a byte stream
@@ -12,13 +13,19 @@
 class StreamReassembler {
   private:
     // Your code here -- add private members as necessary.
-    std::map<size_t, std::string> _unassemble_strs;
-    size_t _next_assembled_idx;
-    size_t _unassembled_bytes_num;
-    size_t _eof_idx;
 
-    ByteStream _output; //!< The reassembled in-order byte stream
-    size_t _capacity;   //!< The maximum number of bytes
+    size_t _capacity;
+    ByteStream _output;
+    std::deque<char> buffer;
+    std::deque<bool> bitmap;
+
+    size_t first_unass;
+    size_t unass_size;
+    bool _eof;
+
+    size_t remain_size() {
+        return _capacity - _output.buffer_size() - unass_size;
+    }
 
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity`
